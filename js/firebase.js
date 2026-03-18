b2d12d73d222753b5326b315b9ff77a5d85f1060
vivekmanickam4 <vivekmanickam4@gmail.com>
2026-03-19 00:31:16 +0400

    Implement Firebase authentication with login function
    Added authentication functionality using Firebase Auth.

--- js/firebase.js
+++ js/firebase.js
@@ -1,22 +1,32 @@
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Firebase imports
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyDU-_8EWlVyfmjjMS3hr2s2Wbs7Cmpa0ZY",
-    authDomain: "fir-console-55101.firebaseapp.com",
-    projectId: "fir-console-55101",
-    storageBucket: "fir-console-55101.firebasestorage.app",
-    messagingSenderId: "344044870689",
-    appId: "1:344044870689:web:b69e6822e3deecc07fd760",
-    measurementId: "G-2CMNXN1T9R"
-  };
+// Config
+const firebaseConfig = {
+  apiKey: "AIzaSyDU-_8EWlVyfmjjMS3hr2s2Wbs7Cmpa0ZY",
+  authDomain: "fir-console-55101.firebaseapp.com",
+  projectId: "fir-console-55101",
+  storageBucket: "fir-console-55101.firebasestorage.app",
+  messagingSenderId: "344044870689",
+  appId: "1:344044870689:web:b69e6822e3deecc07fd760",
+  measurementId: "G-2CMNXN1T9R"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+// Init
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// Login function
+window.login = function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      alert("Login successful");
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+};
