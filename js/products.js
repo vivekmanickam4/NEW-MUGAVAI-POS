ce3d0885914552b5aeadce59dc81d0dddcde87e8
vivekmanickam4 <vivekmanickam4@gmail.com>
2026-03-19 23:31:40 +0400

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,40 +1,30 @@
 import { db } from "./firebase.js";
-import {
-  collection, addDoc, deleteDoc, doc, onSnapshot
-} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { collection, getDocs } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
 
-if(localStorage.getItem("role") !== "admin"){
-  document.getElementById("adminPanel").style.display="none";
-}
+async function loadProducts() {
+  const table = document.getElementById("table");
 
-const col = collection(db,"products");
+  const snap = await getDocs(collection(db, "products"));
 
-window.add = async function(){
-  await addDoc(col,{
-    name:name.value,
-    price:parseFloat(price.value),
-    gst:parseFloat(gst.value),
-    barcode:barcode.value
+  table.innerHTML = `
+    <tr>
+      <th>Name</th>
+      <th>Price</th>
+      <th>GST</th>
+    </tr>
+  `;
+
+  snap.forEach(d => {
+    let p = d.data();
+
+    table.innerHTML += `
+      <tr>
+        <td>${p.name}</td>
+        <td>${p.price}</td>
+        <td>${p.gst}%</td>
+      </tr>
+    `;
   });
 }
 
-window.del = async function(id){
-  await deleteDoc(doc(db,"products",id));
-}
-
-onSnapshot(col, snap=>{
-  table.innerHTML="<tr><th>Name</th><th>Price</th><th>GST</th><th>Action</th></tr>";
-
-  snap.forEach(d=>{
-    let p=d.data();
-
-    table.innerHTML+=`<tr>
-      <td>${p.name}</td>
-      <td>₹${p.price}</td>
-      <td>${p.gst}%</td>
-      <td>
-        ${localStorage.getItem("role")==="admin" ? `<button onclick="del('${d.id}')">Delete</button>` : ""}
-      </td>
-    </tr>`;
-  });
-});
+loadProducts();
