c2fb1a7671a7573f157c99a540c8b01bf68706b5
vivekmanickam4 <vivekmanickam4@gmail.com>
2026-03-19 22:11:04 +0400

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -245,4 +245,7 @@ window.addEventListener("DOMContentLoaded", () => {
   loadProductsForScan();
 });
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
+
 export { db };
