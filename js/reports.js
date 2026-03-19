9f9d7a5219393c9f760c8bf2ecaf6b80884091f2
vivekmanickam4 <vivekmanickam4@gmail.com>
2026-03-19 23:50:06 +0400

    Update reports.js

--- js/reports.js
+++ js/reports.js
@@ -3,15 +3,33 @@ import { collection, getDocs }
 from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
 
 async function load(){
+
   let snap = await getDocs(collection(db,"bills"));
 
-  let total = 0;
+  let today = 0;
+  let month = 0;
+
+  let now = new Date();
 
   snap.forEach(d=>{
-    total += d.data().total;
+    let b = d.data();
+    let date = new Date(b.createdAt.seconds * 1000);
+
+    // TODAY
+    if(date.toDateString() === now.toDateString()){
+      today += b.total;
+    }
+
+    // MONTH
+    if(date.getMonth() === now.getMonth()){
+      month += b.total;
+    }
   });
 
-  document.getElementById("sales").innerText = total.toFixed(2);
+  document.body.innerHTML += `
+    <h2>Today's Sales ₹${today.toFixed(2)}</h2>
+    <h2>Monthly Sales ₹${month.toFixed(2)}</h2>
+  `;
 }
 
 load();
