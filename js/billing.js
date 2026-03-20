20fc58deabc2d41596fb184a83e12554c4297957
vivekmanickam4 <vivekmanickam4@gmail.com>
2026-03-20 18:08:06 +0400

    Update billing.js

--- js/billing.js
+++ js/billing.js
@@ -33,7 +33,15 @@ window.addEventListener("load", () => {
 /* INVOICE */
 function generateInvoice() {
   const now = new Date();
-  return "INV-" + now.getTime();
+
+  const dd = String(now.getDate()).padStart(2, "0");
+  const mm = String(now.getMonth() + 1).padStart(2, "0");
+  const yyyy = now.getFullYear();
+
+  const hh = String(now.getHours()).padStart(2, "0");
+  const min = String(now.getMinutes()).padStart(2, "0");
+
+  return `INV-${dd}${mm}${yyyy}-${hh}${min}`;
 }
 document.getElementById("invNo").innerText = generateInvoice();
 
